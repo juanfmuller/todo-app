97f6eb0148e2e5da89662abf429574ab6d9c1565
juanfmuller <juanfra500@gmail.com>
2021-10-09 15:55:01 -0600

    Added todo form to frontend.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,58 @@
-import React from 'react';
-import react from 'react'
-import './App.css';
+import React from 'react'
+import './App.css'
 
 class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      todoList:[],
+      activeItem:{
+        id:null,
+        title:"",
+        completed:false,
+      },
+      editing:false,
+    }
+    this.fetchTasks = this.fetchTasks.bind(this);
+  };
+
+  componentWillMount(){
+    this.fetchTasks();
+  }
+
+  fetchTasks(){
+    console.log('Fetching...');
+
+    fetch('http://127.0.0.1:8000/api/task-list')
+    .then(response => response.json())
+    .then(data =>
+      console.log('Data:', data)
+      )
+  }
+
   render(){
     return(
       <div className="container">
+        <div id="task-container">
+          <div id="form-wrapper">
+            <form id="form">
+              <div className="flex-wrapper">
+                <div style={{flex: 6}}>
+                  <input className="form-control" id="title" type="text" name="title" placeholder="Enter task"></input>
+                </div>
+                <div style={{flex: 1}}>
+                  <input id="submit" className="btn btn-warning" type="submit" name="Add" ></input>
+                </div>
+              </div>
+            </form>
+          </div>
+          <div id="list-wrapper">
 
+          </div>
+        </div>
       </div>
     )
   }
-
 }
+
+export default App
