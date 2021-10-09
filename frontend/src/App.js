127523f14f6e64ab8dec842b55d6261fce64f3f8
juanfmuller <juanfra500@gmail.com>
2021-10-09 17:54:21 -0600

    Added CRUD functionality to frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,8 +13,30 @@ class App extends React.Component {
       },
       editing:false,
     }
-    this.fetchTasks = this.fetchTasks.bind(this);
+    this.fetchTasks = this.fetchTasks.bind(this)
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.getCookie = this.getCookie.bind(this)
+    this.startEdit = this.startEdit.bind(this)
+    this.deleteItem = this.deleteItem.bind(this)
+    
   };
+
+  getCookie(name) {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
 
   componentWillMount(){
     this.fetchTasks();
@@ -23,22 +45,96 @@ class App extends React.Component {
   fetchTasks(){
     console.log('Fetching...');
 
-    fetch('http://127.0.0.1:8000/api/task-list')
+    fetch('http://127.0.0.1:8000/api/task-list/')
     .then(response => response.json())
     .then(data =>
-      console.log('Data:', data)
+      this.setState({
+        todoList:data.reverse()
+      })
       )
   }
 
+  handleChange(e){
+    var name = e.target.name
+    var value = e.target.value
+    console.log('Name:', name)
+    console.log('Value:', value)
+    this.setState({
+      activeItem:{
+        ...this.state.activeItem,
+        title:value
+      }
+    })
+  }
+
+  handleSubmit(e){
+    e.preventDefault()
+    console.log('ITEM:', this.state.activeItem)
+    
+    var csrftoken = this.getCookie('csrftoken')
+
+    var url = 'http://127.0.0.1:8000/api/task-create/'
+    if(this.state.editing){
+      url = `http://127.0.0.1:8000/api/task-update/${this.state.activeItem.id}`
+      this.setState({
+        editing:false
+      })
+    }
+
+    fetch(url, {
+      method: 'POST',
+      headers:{
+        'Content-type':'application/json',
+        'X-CSRFToken': csrftoken,
+      },
+      body:JSON.stringify(this.state.activeItem)
+    }).then(response => {
+      this.fetchTasks()
+      this.setState({
+        activeItem:{
+          id:null,
+          title:"",
+          completed:false,
+        }
+      })
+    }).catch(function(error){
+      console.log('ERROR:', error)
+    })
+  }
+
+  startEdit(task){
+    this.setState({
+      activeItem:task,
+      editing:true
+    })
+  }
+
+  deleteItem(task){
+    let csrftoken = this.getCookie('csrftoken')
+
+    fetch(`http://127.0.0.1:8000/api/task-delete/${task.id}`, {
+      method:'DELETE',
+      headers:{
+        'Content-type':'application/json',
+        'X-CSRFToken':csrftoken
+      },
+    }).then(response => {
+      this.fetchTasks()
+    })
+  }
+
   render(){
+    var tasks = this.state.todoList;
+    var self = this;
+
     return(
       <div className="container">
         <div id="task-container">
           <div id="form-wrapper">
-            <form id="form">
+            <form onSubmit={this.handleSubmit} id="form">
               <div className="flex-wrapper">
                 <div style={{flex: 6}}>
-                  <input className="form-control" id="title" type="text" name="title" placeholder="Enter task"></input>
+                  <input onChange={this.handleChange} className="form-control" id="title"  value={this.state.activeItem.title} type="text" name="title" placeholder="Enter task"></input>
                 </div>
                 <div style={{flex: 1}}>
                   <input id="submit" className="btn btn-warning" type="submit" name="Add" ></input>
@@ -47,7 +143,21 @@ class App extends React.Component {
             </form>
           </div>
           <div id="list-wrapper">
-
+            {tasks.map(function(task, index){
+              return(
+                <div key={index} className="task-wrapper flex-wrapper">
+                  <div style={{flex:7}}>
+                    <span>{task.title}</span>
+                  </div>
+                  <div style={{flex:1}}>
+                    <button onClick={() => self.startEdit(task)} className="btn btn-sm btn-outline-info">Edit</button>
+                  </div>
+                  <div style={{flex:1}}>
+                  <button onClick={() => self.deleteItem(task)} className="btn btn-sm btn-outline-dark delete">-</button>
+                  </div>
+                </div>
+              )
+            })}
           </div>
         </div>
       </div>
